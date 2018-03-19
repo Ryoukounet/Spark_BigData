2f301ab2011b00b3985ff2de426eb056d41e9680
Ryoukounet <alex92250@gmail.com>
2018-03-20 00:34:45 +0100

    Update Q1.scala

--- Q1.scala
+++ Q1.scala
@@ -1,13 +1,4 @@
-/* val start = System.nanoTime
-val data =sc.textFile("/home/ryouk/Documents/spark/Crimes-2001-present.csv")
-val mappedData =data.map(line => line.split(","))
-val selectedData =mappedData.map(line=> (line(5),1))
-val reducedData =selectedData.reduceByKey(_+_)
-val sortedData =reducedData.sortBy(_._2)
-sortedData.coalesce(1).saveAsTextFile("/home/ryouk/Documents/spark/question1")
-val time = (System.nanoTime - start) / 1e9d
-println(duration + " s")
-*/
+
 
 val start = System.nanoTime
 sc.textFile("/home/ryouk/Documents/spark/Crimes-2001-present.csv").map(line => line.split(",")).map(line=> (line(5),1)).reduceByKey(_+_).sortBy(_._2).coalesce(1).saveAsTextFile("/home/ryouk/Documents/spark/question_1_full")
